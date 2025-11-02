ee5cc30f0c2ed384a5261365a89da9ebc1f03fdb
Isabella Szabo <144571520+iszabo0150@users.noreply.github.com>
2025-11-02 14:46:10 -0500

    fixed switch

--- Game.java
+++ Game.java
@@ -23,6 +23,10 @@ public class Game {
                 break;
             case DOWN:
                 this.currCloud.moveDown();
+                break;
+            default:
+                break;
         }
+        e.consume();
     }
 }
