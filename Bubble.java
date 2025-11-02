3be08d247a10eeac121a105af8ecf40f182bc2ae
Isabella Szabo <144571520+iszabo0150@users.noreply.github.com>
2025-11-02 15:01:35 -0500

    fixed sol - isabella

--- Bubble.java
+++ Bubble.java
@@ -4,24 +4,20 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 
 public class Bubble {
-    Circle bubble;
-    public Bubble(Pane pane){
-        this.createBubble(pane);
+
+    private Circle bubble;
+
+    public Bubble(Pane pane, int x, int y) {
+        this.createBubble(pane, x, y);
     }
 
-    private void createBubble(Pane pane){
-        this.bubble = new Circle();
-        this.bubble.setRadius(Constants.BUBBLE_SIZE);
+    private void createBubble(Pane pane, int x, int y) {
+        this.bubble = new Circle(x, y, Constants.BUBBLE_SIZE);
         this.bubble.setFill(Constants.BUBBLE_COLOR);
         pane.getChildren().add(this.bubble);
     }
 
-    public void setCoordinates(int x, int y){
-        this.bubble.setCenterX(x);
-        this.bubble.setCenterY(y);
-    }
-
-    public int[] getCoordinates(){
-        return new int[]{(int) this.bubble.getCenterX(), (int) this.bubble.getCenterY()};
+    public void moveRight() {
+        this.bubble.setCenterX(this.bubble.getCenterX() + Constants.BUBBLE_OFFSET);
     }
 }
