d1012f58c7306bd7916805f2a893efabec1d390e
Isabella Szabo <144571520+iszabo0150@users.noreply.github.com>
2025-11-02 15:02:05 -0500

    fixed sol

--- Cloud.java
+++ Cloud.java
@@ -3,32 +3,25 @@ package codeAlong6;
 import javafx.scene.layout.Pane;
 
 public class Cloud {
-    int[][] coordinates;
-    Bubble[] bubbles;
-    public Cloud(int[][] coords, Pane pane){
-        this.coordinates = coords;
-        this.bubbles = new Bubble[Constants.num_bubbles];
-        this.generateCloud(pane);
-        this.arrangeBubbles();
+
+    private Bubble[] bubbles;
+
+    public Cloud(int[][] coords, Pane pane) {
+        this.bubbles = new Bubble[Constants.NUM_BUBBLES];
+        this.generateBubbles(pane, coords);
     }
 
-    private void generateCloud(Pane pane){
-        for(int i = 0; i < this.coordinates.length; i++){
-            this.bubbles[i] = new Bubble(pane);
+    private void generateBubbles(Pane pane, int[][] coords) {
+        for (int i = 0; i < this.bubbles.length; i++) {
+            int xCoord = coords[i][0];
+            int yCoord = coords[i][1];
+            this.bubbles[i] = new Bubble(pane, xCoord, yCoord);
         }
     }
 
-    private void arrangeBubbles(){
-        for(int i = 0; i < this.coordinates.length; i++){
-            this.bubbles[i].setCoordinates(
-                    this.coordinates[i][Constants.x_coord],this.coordinates[i][Constants.y_coord]);
-        }
-    }
-
-    public void moveDown(){
-        for(int i = 0; i < this.bubbles.length; i++){
-            this.bubbles[i].setCoordinates(this.bubbles[i].getCoordinates()[Constants.x_coord],
-                                            this.bubbles[i].getCoordinates()[Constants.y_coord] + 5);
+    public void moveRight() {
+        for (Bubble bubble: this.bubbles) {
+            bubble.moveRight();
         }
     }
 }
